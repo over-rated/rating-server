aeb984f4e952dfad975aada3c2d5c9f86f73bdf3
chellxchell <rochellecompendio2021@u.northwestern.edu>
2021-05-25 18:12:50 -0500

    changed login route so it also returns the user object

--- src/routes.js
+++ src/routes.js
@@ -35,10 +35,22 @@ router.route("/users")
     .get((_req, res) => {
         console.log("GET /users");
         const users = User.find({})
-        .then(users => {
-            res.status(200).send({users});
-        });
+            .then(users => {
+                res.status(200).send({ users });
+            });
 
+    });
+
+router.route("/users/:id")
+    .get((req, res) => {
+        console.log(`GET /users/${req.params.id}`);
+        User.findById(req.params.id)
+            .then(data => {
+                res.status(200).send(data)
+            })
+            .catch(err => {
+                res.status(404).send("user not found")
+            });
     });
 
 router.route("/login")
@@ -50,17 +62,16 @@ router.route("/login")
         // Query for user and if authenticated, return jwt
         if (!username || !password) {
             res.status(400).send({
-                "message" : "Bad Request."
+                "message": "Bad Request."
             });
             return;
         }
 
-        var user = await User.findOne({username: username});
-        console.log(user);
+        var user = await User.findOne({ username: username });
 
-        if (!user || user.password != password){
+        if (!user || user.password != password) {
             res.status(400).send({
-                "message" : "Incorrect username or password"
+                "message": "Incorrect username or password"
             })
             return;
         }
@@ -69,10 +80,13 @@ router.route("/login")
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '600s' }
         );
-
+        
+        // chelly added this idk if this is allowed but i need this for login lol
         res.status(200).send({
-            accessToken : token
+            accessToken: token,
+            ...user
         });
+        console.log("token", token)
 
     });
 
@@ -83,32 +97,32 @@ router.route("/signup")
         const password = req.body.password;
         const name = req.body.name;
 
-        if (!username || !password || !name){
+        if (!username || !password || !name) {
             res.status(400).send({
-                "Message" : "Missing required fields."
+                "Message": "Missing required fields."
             });
             return;
         }
-        else{
-            var existingUser = await User.findOne({"username" : username});
-            if (existingUser){
+        else {
+            var existingUser = await User.findOne({ "username": username });
+            if (existingUser) {
                 res.status(400).send({
-                    "Message" : "Username already exists"
+                    "Message": "Username already exists"
                 });
                 return;
             }
             var user = {
-                username : username,
+                username: username,
                 password: password,
                 name: name
             };
-            if (req.body.bio){
+            if (req.body.bio) {
                 user.bio = req.body.bio;
             }
             User.create(user).save()
-            .then(newUser => {
-                res.status(201).send(newUser);
-            })
+                .then(newUser => {
+                    res.status(201).send(newUser);
+                })
         }
 
     })
